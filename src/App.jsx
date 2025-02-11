9bb601822b01bc183fa38d34afc5353bbaf610fd
PC <PC@DESKTOP-BF3STLU>
2025-02-11 12:20:56 +0530

    changed app

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     { id: 2, name: 'Bella', type: 'Cat', age: '1', location: 'California', description: 'Loves cuddles', image: cat },
     { id: 3, name: 'Tomy', type: 'Mouse', age: '2', location: 'China', description: 'Loves parties', image: mouse },
     { id: 4, name: 'Chomu', type: 'Tiger', age: '3', location: 'India', description: 'Loves non-veg', image: tiger},
-    { id: 5, name: 'Mangu', type: 'Elephant', age: '5', location: 'Indonesia', description: 'Loves water', image: elephant}
+    { id: 5, name: 'Suraj', type: 'Elephant', age: '5', location: 'Indonesia', description: 'Loves water', image: elephant}
     
     
   ];
