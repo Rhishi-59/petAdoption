155dc11cb96c47383da170c3c262ae8c0357c9c2
PC <PC@DESKTOP-BF3STLU>
2025-02-11 10:40:48 +0530

    updated home.css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import PetCard from '../components/PetCard';
 import PetFilter from '../components/PetFilter';
+import './Home.css';
 
 function Home({petData}) {
   const [pets, setPets] = useState([]);
@@ -14,7 +15,7 @@ function Home({petData}) {
   }, []);
 
   return (
-    <div>
+    <div className='home'>
       <h1>Pet Adoption</h1>
       <PetFilter pets={pets} setFilteredPets={setFilteredPets} />
       <div className="pet-list">
